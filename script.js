1f3cdb7ddad1392db25243bf8c6a1ef3e6924f9d
oybek003 <ojbekmuslimov1@gmail.com>
2024-12-14 13:14:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let name = "userName";
+let userName = prompt('String typedagi malumot kiriting');
+console.log(typeof userName);
+
+let number = "age";
+let age = prompt('Number typedagi malumot kiriting');
+console.log(typeof 42);
+
+
+let isActive = true;
+let isComplete = false;
+let Boolean = prompt('Boolean typedagi malumot kiriting');
+console.log(typeof true);
+
+
+alert(`hamma malumotlarni togri kiritingizmi`);
+
+alert(`siz kiritgan ${userName} sozni string typiga tegishli \n siz kiritgan${age} sozni number typiga tegishli \n siz kiritgan ${Boolean} sozni boolean typiga tegishli`);
